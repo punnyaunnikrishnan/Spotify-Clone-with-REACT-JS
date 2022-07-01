0081e3b26da4ff00df230197d03bd198c946d97b
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-01 17:50:14 +0530

    spotify logo added

--- spotify_clone/src/Login.js
+++ spotify_clone/src/Login.js
@@ -4,6 +4,10 @@ function Login() {
   return (
     <div className="login">
       <h1>I'm the Login page</h1>
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
     </div>
   );
 }
