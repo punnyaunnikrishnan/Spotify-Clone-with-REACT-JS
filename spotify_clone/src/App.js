29bce85f7db702b4fa03bc25943f937003face52
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-01 16:38:21 +0530

    rendered login component

---
+++ spotify_clone/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Spotify build</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
