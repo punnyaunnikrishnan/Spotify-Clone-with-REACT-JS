739c98a7d9d6fa6567756f2d73ceb4f180ac9020
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-01 18:07:32 +0530

    modified

--- spotify_clone/src/Login.js
+++ spotify_clone/src/Login.js
@@ -8,7 +8,7 @@ function Login() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
-      <button>LOGIN WITH SPOTIFY</button>
+      <a>LOGIN WITH SPOTIFY</a>
     </div>
   );
 }
