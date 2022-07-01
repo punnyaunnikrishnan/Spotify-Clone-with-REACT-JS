3859add93464e1621845ccc0a24c4b5939e80ae4
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-01 18:24:40 +0530

    added scopes

--- spotify_clone/src/spotify.js
+++ spotify_clone/src/spotify.js
@@ -1,3 +1,10 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 const redirectUri = "http://localhost:3000/";
 const clientId = "46ea7bfe06204fa4a1003d6e09327663";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
