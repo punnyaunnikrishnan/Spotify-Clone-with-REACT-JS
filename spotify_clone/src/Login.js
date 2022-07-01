7c8ed65bc0b7e085f4ebab667fa97479858008ba
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-01 16:50:47 +0530

    Login.js

---
+++ spotify_clone/src/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Login.css";
+function Login() {
+  return (
+    <div className="login">
+      <h1>I'm the Login page</h1>
+    </div>
+  );
+}
+
+export default Login;
