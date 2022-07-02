af0ff6d8a1f0d19aadd14f56f9e45605f96b5089
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-02 21:01:58 +0530

    App.js

--- spotify_clone/src/App.js
+++ spotify_clone/src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState();
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const token = hash.access_token;
+    console.log("I have a token", token);
+  }, []);
   return (
     <div className="app">
       <Login />
