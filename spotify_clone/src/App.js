7f4909ccd461c4891f47854385fa9b5d7579960f
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-04 10:19:32 +0530

    App.js

--- spotify_clone/src/App.js
+++ spotify_clone/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
 const spotify = new SpotifyWebApi();
 function App() {
   const [token, setToken] = useState(null);
