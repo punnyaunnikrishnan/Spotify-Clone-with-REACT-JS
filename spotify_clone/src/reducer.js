98922421c6c5c1cdfeeabfecfa56fabba0107203
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-05 14:58:28 +0530

    reducer.js

---
+++ spotify_clone/src/reducer.js
@@ -0,0 +1,21 @@
+import { act } from "react-dom/test-utils";
+
+export const initialState = {
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+};
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    default:
+      return state;
+  }
+};
+export default reducer;
