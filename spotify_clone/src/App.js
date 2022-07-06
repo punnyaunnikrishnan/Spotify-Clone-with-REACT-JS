64a572eca93de54c722d02313d8b29e51b902caa
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-06 17:29:30 +0530

    App.js

--- spotify_clone/src/App.js
+++ spotify_clone/src/App.js
@@ -4,17 +4,19 @@ import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from "./Player";
-import { useDataLayerValue } from "./DataLayer";
+import { DataLayer, useDataLayerValue } from "./DataLayer";
 const spotify = new SpotifyWebApi();
 function App() {
-  const [token, setToken] = useState(null);
-  const [{ user }, dispatch] = useDataLayerValue();
+  const [{ user, token }, dispatch] = useDataLayerValue();
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
       spotify.setAccessToken(_token);
       spotify.getMe().then((user) => {
         console.log(user);
