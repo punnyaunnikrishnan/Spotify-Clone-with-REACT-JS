793c87d48c5ab872cf2aa2bff5af3f0db017f584
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-06 19:44:39 +0530

    Sidebar.js

--- spotify_clone/src/Sidebar.js
+++ spotify_clone/src/Sidebar.js
@@ -4,7 +4,7 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <img
-        className="sidebar_logo"
+        className="sidebar__logo"
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
