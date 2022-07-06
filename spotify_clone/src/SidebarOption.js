8c8ec782b658dfb7d86105ebc6d14259003b47ff
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-06 19:55:06 +0530

    modified

--- spotify_clone/src/SidebarOption.js
+++ spotify_clone/src/SidebarOption.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./SidebarOption.css";
 
 function SidebarOption({ title, Icon }) {
-  return <div className="sidebarOption"></div>;
+  return (
+    <div className="sidebarOption">
+      <p>{title}</p>
+    </div>
+  );
 }
 
 export default SidebarOption;
