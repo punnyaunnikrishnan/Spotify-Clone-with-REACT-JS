123a28575b3bdc4a9e9e958470295ce2a47abdf2
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-06 19:48:47 +0530

    Sidebar props passed

---
+++ spotify_clone/src/SidebarOption.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SidebarOption.css";
+
+function SidebarOption({ title, Icon }) {
+  return <div className="sidebarOption"></div>;
+}
+
+export default SidebarOption;
