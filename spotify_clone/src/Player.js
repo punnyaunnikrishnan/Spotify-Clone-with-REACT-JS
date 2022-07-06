fa699e59a998fb0889f07f1939a052b36a534215
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-06 18:38:54 +0530

    Player.js

--- spotify_clone/src/Player.js
+++ spotify_clone/src/Player.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Player.css";
+import Sidebar from "./Sidebar";
 
 function Player({ spotify }) {
   return (
     <div className="player">
-      <div className="player_body"></div>
+      <div className="player_body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
