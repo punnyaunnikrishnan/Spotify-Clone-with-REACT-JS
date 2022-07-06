a404db21fd3a8a9bfe780302afb57eb1bba7d555
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-06 18:57:19 +0530

    footer component added

--- spotify_clone/src/Player.js
+++ spotify_clone/src/Player.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Player.css";
 import Sidebar from "./Sidebar";
 import Body from "./Body";
+import Footer from "./Footer";
 
 function Player({ spotify }) {
   return (
@@ -10,6 +11,7 @@ function Player({ spotify }) {
         <Sidebar />
         <Body />
       </div>
+      <Footer />
     </div>
   );
 }
