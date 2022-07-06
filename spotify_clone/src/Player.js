e070b588813250dba95a49cf5fc9781185daaf88
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-06 18:32:05 +0530

    Player.js

--- spotify_clone/src/Player.js
+++ spotify_clone/src/Player.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Player.css";
 
 function Player({ spotify }) {
   return (
-    <div>
-      <h1>Welcome to Spotify</h1>
+    <div className="player">
+      <div className="player_body"></div>
     </div>
   );
 }
