143092b14c07097629305a12814d2a83e85a2100
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-09 17:04:22 +0530

    Body.js

--- spotify_clone/src/Body.js
+++ spotify_clone/src/Body.js
@@ -6,6 +6,13 @@ function Body({ spotify }) {
   return (
     <div className="body">
       <Header spotify={spotify} />
+      <div className="body__info">
+        <img src="" alt="" />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+        </div>
+      </div>
     </div>
   );
 }
