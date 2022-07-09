7b4d4a585c71bb76f30a9e11af6dd2fb482bc9cd
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-09 11:57:12 +0530

    footer icons imported

--- spotify_clone/src/Footer.js
+++ spotify_clone/src/Footer.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
 function Footer() {
   return (
     <div className="footer">
@@ -7,7 +12,11 @@ function Footer() {
         <h1>Album and song details</h1>
       </div>
       <div className="footer__center">
-        <h1>Player controls</h1>
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+        <SkipNextIcon fontSize="large" className="footer__icon" />
+        <RepeatIcon className="footer__green" />
       </div>
       <div className="footer__right">
         <h1>Volume controls</h1>
