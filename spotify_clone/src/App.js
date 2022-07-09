9eaef7c7dfcea06c5899b236acbc2814a21963c1
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-09 22:07:00 +0530

    App.js

--- spotify_clone/src/App.js
+++ spotify_clone/src/App.js
@@ -30,6 +30,12 @@ function App() {
           playlists: playlists,
         });
       });
+      spotify.getPlaylist("37i9dQZEVXcIm7Gt8PEd09").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
     }
   }, []);
 
