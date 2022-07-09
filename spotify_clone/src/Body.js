d68e7a9e01941eb2b5ad6847d636b133ec8413f8
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-09 22:28:06 +0530

    Body.js

--- spotify_clone/src/Body.js
+++ spotify_clone/src/Body.js
@@ -9,11 +9,11 @@ function Body({ spotify }) {
     <div className="body">
       <Header spotify={spotify} />
       <div className="body__info">
-        <img src="" alt="" />
+        <img src={discover_weekly?.images[0].url} alt="" />
         <div className="body__infoText">
           <strong>PLAYLIST</strong>
           <h2>Discover Weekly</h2>
-          <p>description...</p>
+          <p>{discover_weekly?.description}</p>
         </div>
       </div>
     </div>
