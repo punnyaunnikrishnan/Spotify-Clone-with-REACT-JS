d1b4a1f0a6b402c130a89ed20201cb02196e34a3
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-10 19:32:12 +0530

    spotify.js

--- spotify_clone/src/spotify.js
+++ spotify_clone/src/spotify.js
@@ -1,6 +1,8 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectUri = "http://localhost:3000/";
+// Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "46ea7bfe06204fa4a1003d6e09327663";
+const redirectUri = "http://localhost:3000/";
 const scopes = [
   "user-read-currently-playing",
   "user-read-recently-played",
@@ -8,16 +10,19 @@ const scopes = [
   "user-top-read",
   "user-modify-playback-state",
 ];
-export const getTokenFromUrl = () => {
+
+export const getTokenFromResponse = () => {
   return window.location.hash
     .substring(1)
     .split("&")
     .reduce((initial, item) => {
-      let parts = item.split("=");
+      var parts = item.split("=");
       initial[parts[0]] = decodeURIComponent(parts[1]);
+
       return initial;
     }, {});
 };
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
