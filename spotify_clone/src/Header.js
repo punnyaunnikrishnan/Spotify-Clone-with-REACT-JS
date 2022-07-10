df94ee89f36ca93e67d41faa86801bffc227663d
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-10 18:35:28 +0530

    Header.js

--- spotify_clone/src/Header.js
+++ spotify_clone/src/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Header.css";
-import SearchIcon from "@material-ui/icons/Search";
+import { useStateValue } from "./StateProvider";
 import { Avatar } from "@material-ui/core";
-import { useDataLayerValue } from "./DataLayer";
-function Header() {
-  const [{ user }, dispatch] = useDataLayerValue();
+import SearchIcon from "@material-ui/icons/Search";
+import { useEffect } from "react";
+
+function Header({ spotify }) {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="header">
       <div className="header__left">
@@ -15,7 +18,7 @@ function Header() {
         />
       </div>
       <div className="header__right">
-        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+        <Avatar alt={user?.display_name} src={user?.images[0].url} />
         <h4>{user?.display_name}</h4>
       </div>
     </div>
