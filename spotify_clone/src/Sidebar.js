320691c5c020bbecbe6118a93525b00186b476aa
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-10 19:31:06 +0530

    Sidebar.js

--- spotify_clone/src/Sidebar.js
+++ spotify_clone/src/Sidebar.js
@@ -4,9 +4,13 @@ import SidebarOption from "./SidebarOption";
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
-import { useDataLayerValue } from "./DataLayer";
+import { getTokenFromResponse } from "./spotify";
+import { useStateValue } from "./StateProvider";
+
 function Sidebar() {
-  const [{ playlists }, dispatch] = useDataLayerValue();
+  const [{ playlists }, dispatch] = useStateValue();
+  console.log(playlists);
+
   return (
     <div className="sidebar">
       <img
@@ -14,18 +18,15 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
-      <SidebarOption Icon={HomeIcon} title="Home" />
-      <SidebarOption Icon={SearchIcon} title="Search" />
-      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+      <SidebarOption Icon={HomeIcon} option="Home" />
+      <SidebarOption Icon={SearchIcon} option="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} option="Your Library" />
       <br />
       <strong className="sidebar__title">PLAYLISTS</strong>
       <hr />
       {playlists?.items?.map((playlist) => (
-        <SidebarOption title={playlist.name} />
+        <SidebarOption option={playlist.name} />
       ))}
-      <SidebarOption title="Hip Hop" />
-      <SidebarOption title="Rock" />
-      <SidebarOption title="RnB" />
     </div>
   );
 }
