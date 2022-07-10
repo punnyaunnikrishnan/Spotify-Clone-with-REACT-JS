954c2c283102c6ee37b5c4b1b3f549b9a6924fd1
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-10 18:37:30 +0530

    reducer.js

--- spotify_clone/src/reducer.js
+++ spotify_clone/src/reducer.js
@@ -1,9 +1,15 @@
+import { findAllByDisplayValue } from "@testing-library/react";
+
 export const initialState = {
   user: null,
   playlists: [],
+  spotify: null,
+  discover_weekly: null,
+  top_artists: null,
   playing: false,
   item: null,
 };
+
 const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
@@ -12,24 +18,51 @@ const reducer = (state, action) => {
         ...state,
         user: action.user,
       };
-    case "SET_TOKEN":
+
+    case "SET_PLAYING":
       return {
         ...state,
-        token: action.token,
+        playing: action.playing,
       };
-    case "SET_PLAYLISTS":
+
+    case "SET_ITEM":
       return {
         ...state,
-        playlists: action.playlists,
+        item: action.item,
       };
+
     case "SET_DISCOVER_WEEKLY":
       return {
         ...state,
         discover_weekly: action.discover_weekly,
       };
 
+    case "SET_TOP_ARTISTS":
+      return {
+        ...state,
+        top_artists: action.top_artists,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+
+    case "SET_SPOTIFY":
+      return {
+        ...state,
+        spotify: action.spotify,
+      };
+
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
     default:
       return state;
   }
 };
+
 export default reducer;
