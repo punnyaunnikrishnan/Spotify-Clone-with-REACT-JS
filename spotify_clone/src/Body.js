5d2b6c869341adaae6b35fb70b1c1bf1db3895de
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-10 17:57:38 +0530

    Body.js

--- spotify_clone/src/Body.js
+++ spotify_clone/src/Body.js
@@ -5,6 +5,7 @@ import { useDataLayerValue } from "./DataLayer";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import SongRow from "./SongRow";
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useDataLayerValue();
   return (
